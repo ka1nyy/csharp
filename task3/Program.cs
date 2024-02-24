61978e442a906fbdf9210a9144d095e53069a645
Arnur <kainyvlog360@gmail.com>
2024-02-24 22:33:31 +0600

    find the most valuable number

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,12 +1,25 @@
 ﻿int a = 5;
 int b = 7;
+int c = 43;
+int d = 12;
+int e = 9;
 
-if (a < b)
-{
-        Console.WriteLine(b);
-}
-else 
-{
-    Console.WriteLine(a);
+int max = a;
 
+if (b > max)
+{
+     max = b;
 }
+if (c > max)
+{
+     max = c;
+}
+if (d > max)
+{
+     max = d;
+}
+if (e > max)
+{
+     max = e;
+}
+Console.WriteLine(max);
