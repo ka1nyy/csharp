969180ba6afba286256a5376fc399c5bbee3bd06
Arnur <kainyvlog360@gmail.com>
2024-02-24 21:24:20 +0600

    add some variables - lesson1:task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("hello world");
+﻿using System.Diagnostics;
+
+//Console.WriteLine("hello world");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '1';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
